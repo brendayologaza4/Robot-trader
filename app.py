950bac284f4cc49db1f5785706224c539d61843a
brendayologaza4 <davidyologaza4@outlook.com>
2025-10-25 20:58:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 import random
 import numpy as np
 import requests
-ADMIN_USERNAME = "Brendabella"
+ADMIN_USERNAME = "BrendaBella1"
 NOWPAYMENTS_API_KEY = "CADPW8X-HAJ4NE1-GESD40K-BE8YE8E"
 
 # --- Initialisation Flask ---
